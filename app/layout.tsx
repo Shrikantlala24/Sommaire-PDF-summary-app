a92282d7540e316ee25f2f93c003432221e7f4ba
Shrikantlala24 <shrikantlala2023@gmail.com>
2025-09-05 01:34:49 +0530

    project setup
    project environment is all set :-
    - next.js
    - shadcn UI
    - ESlint
    - prettier

---
+++ app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+import { Source_Sans_3 as FontSans } from "next/font/google";
+import "./globals.css";
+
+const fontSans = FontSans({
+  variable: "--font-sans",
+  subsets: ["latin"],
+  weight: ['200','300','400','500','600','700','800','900']
+});
+
+
+export const metadata: Metadata = {
+  title: "Sommaire",
+  description: "Sommaire - an AI PDF summarizer",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${fontSans.variable} fontSans antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
